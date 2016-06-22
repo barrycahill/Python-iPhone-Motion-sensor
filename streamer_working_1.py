41b0a45b0ec0447acf79ea96fed3942fcb0d7f13
barrycahill <barry.cahill@gmail.com>
2016-06-22 21:08:19 +0100

    adding IP confirming code

--- streamer_working_1.py
+++ streamer_working_1.py
@@ -6,6 +6,7 @@ UDP_IP = "192.168.1.17"    #replace with current IP from settings
 UDP_PORT = 5006
 buffer_size = 110   # buffer size is 110 bytes
 
+print 'Current using IP address: ',  UDP_IP, 'Port : ', UDP_PORT
 sock = socket.socket(socket.AF_INET,   # Internet
                      socket.SOCK_DGRAM)    # UDP
 sock.bind((UDP_IP, UDP_PORT))
---
+++ test_getIP.py
@@ -0,0 +1,5 @@
+import netifaces as ni
+ni.ifaddresses('en0')
+ip = ni.ifaddresses('en0')[2][0]['addr']
+print ip  
+# this one is working now
